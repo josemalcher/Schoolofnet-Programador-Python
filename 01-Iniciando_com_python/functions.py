a640d10772049a56e393241cb3f303dea5c82afc
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-12-01 07:13:19 -0300

    10 - Iniciando com funções

---
+++ 01-Iniciando_com_python/functions.py
@@ -0,0 +1,19 @@
+def call_numbers():
+    for number in range(0,10):
+        print(number)
+
+#call_numbers()
+
+def call_numbers_Limit(limit):
+    #list = range(0,10)
+    for number in range(limit):
+        print(number)
+
+#call_numbers_Limit(5)
+
+
+def call_numbers_with_limit(limit):
+    for number in range(10, limit):
+        print(number)
+
+call_numbers_with_limit(50)
