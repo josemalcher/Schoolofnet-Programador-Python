112d2d950eecbb41e7e49873b1d17f27dc0a88ae
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-12-21 12:10:03 -0300

    06 - Lógica de programação na autenticidade da conta bancária

--- 02-Python_Iniciando_na_pratica/main.py
+++ 02-Python_Iniciando_na_pratica/main.py
@@ -1,4 +1,5 @@
 import getpass
+
 print("")
 print("********************************************************")
 print("******************* Caixa Eletrônico *******************")
@@ -8,22 +9,45 @@ print("")
 accont_typed = input("Digite sua conta:  ")
 password_typed = getpass.getpass("Digite sua Senha:  ")
 
-accont_list = {
-    '0001-01' : {
-        'password' : '123456',
-        'name' : 'José Malcher Jr',
+# accont_list = {
+#     '0001-01' : {
+#         'password' : '123456',
+#         'name' : 'José Malcher Jr',
+#         'value': 0
+#     },
+#     '0002-02': {
+#         'password': '123',
+#         'name': 'Luci Souza',
+#         'value': 0
+#     }
+# }
+acconts_list = [
+    {
+        'agency': '0001-01',
+        'password': '123456',
+        'name': 'José Malcher Jr',
         'value': 0
     },
-    '0002-02': {
+    {
+        'agency': '0002-02',
         'password': '123',
         'name': 'Luci Souza',
         'value': 0
     }
-}
+]
+#
+#if accont_typed in accont_list and password_typed == accont_list[accont_typed]['password']:
+#     print('Conta Valida')
+#     print(accont_typed)
+#     # print(password_typed)
+# else:
+#     print('Conta INVALIDA!')
 
-if accont_typed in accont_list and password_typed == accont_list[accont_typed]['password'] :
-    print('Conta Valida')
-    print(accont_typed)
-    #print(password_typed)
-else:
-    print('Conta INVALIDA!')
+flag = False
+for account in acconts_list:
+    if accont_typed == account['agency'] and password_typed == account['password']:
+        flag = True
+        print("Conta Válida")
+        print(accont_typed)
+if not flag:
+    print("CONTA INVALIDA!")
